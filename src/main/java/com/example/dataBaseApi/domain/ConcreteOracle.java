313638f70730b89764bd056bfe0f426189e45444
eramo1 <eramo@unicesar.edu.co>
2024-10-02 03:11:42 -0500

    add crud of oracle

--- src/main/java/com/example/dataBaseApi/domain/ConcreteOracle.java
+++ src/main/java/com/example/dataBaseApi/domain/ConcreteOracle.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class ConcreteOracle implements IDataBaseFactory{
     @Override
     public IDataBase createDataBase(List<Producto> _productos) {
-        return new MySql(_productos);
+        return new Oracle(_productos);
     }
 }
--- src/main/java/com/example/dataBaseApi/domain/Oracle.java
+++ src/main/java/com/example/dataBaseApi/domain/Oracle.java
@@ -3,30 +3,42 @@ package com.example.dataBaseApi.domain;
 import java.util.List;
 
 public class Oracle implements IDataBase{
+    private List<Producto> productos;
+
+    public Oracle(List<Producto> productos) {
+        this.productos = productos;
+    }
 
     @Override
     public void insertarRegistro(Producto producto) {
-
+        productos.add(producto);
     }
 
     @Override
     public List<Producto> consultarRegistro() {
-        return List.of();
+        return productos;
     }
 
 
     @Override
     public void actualizarRegistro(Producto producto) {
-
+        Producto _producto=consultarRegistroId(producto.getId());
+        eliminarRegistro(_producto);
+        insertarRegistro(producto);
     }
 
     @Override
     public void eliminarRegistro(Producto producto) {
-
+        productos.remove(producto);
     }
 
     @Override
     public Producto consultarRegistroId(String id) {
+        for(Producto producto:productos){
+            if (producto.getId().equals(id)){
+                return producto;
+            }
+        }
         return null;
     }
 }
